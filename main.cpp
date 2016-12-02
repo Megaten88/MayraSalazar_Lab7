8ae2cc763afd9c0f89aed07d9d3d126686f4b35e
Mayra Salazar <mayrasalazar@unitec.edu>
2016-12-02 17:38:41 -0600

    Leyeeeendo bitches

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ vector<User*>users;
 void writeFile();
 void readAdmin();
 void readManagers();
+void readInterns();
+void readSupervisors();
 int main(int argc, char const *argv[])
 {
 	/*int opcion;
@@ -22,13 +24,24 @@ int main(int argc, char const *argv[])
 	cin>>opcion;
 	*/
 	users.push_back(new Manager("ewe", "holita@hola.com", "ewewewewewewwewe",22.5));
-	users.push_back(new Intern("nana","asja","mayrasal",3));
+	users.push_back(new Manager("nana","asja","mayrasal44444444",3.5));
 	writeFile();
 	for (int i = 0; i < users.size(); ++i){
 	    users.erase(users.begin() + i);
 	}
     users.clear();
-    
+    readAdmin();
+    readManagers();
+
+    for (int i = 0; i < users.size(); ++i)
+    {
+    	cout<<users.at(i)->toString()<<endl;
+    	cout<<users.at(i)->getName()<<endl;
+    }
+    for (int i = 0; i < users.size(); ++i){
+	    users.erase(users.begin() + i);
+	}
+    users.clear();
 	return 0;
 }
 void writeFile(){
@@ -83,6 +96,46 @@ void readAdmin(){
 	admins.close();
 }
 void readManagers(){
+	string salario;
+	string name;
+	string correo;
+	string password;
+	ifstream managers;
+	managers.open("Managers.txt");
+	if(managers.fail()){
+		cout<<"Falló el archivo."<<endl;
+	}else{
+		while(!managers.eof()){
+			getline(managers, name,';');
+			getline(managers, correo,';');
+			getline(managers, password,';');
+			getline(managers, salario,';');
+			users.push_back(new Manager(name,correo,password,atof(salario.c_str())));
+		}
+	}
+	managers.close();
+}
+void readInterns(){
+	string dias;
+	string name;
+	string correo;
+	string password;
+	ifstream interns;
+	interns.open("Interns.txt");
+	if(interns.fail()){
+		cout<<"Falló el archivo."<<endl;
+	}else{
+		while(!interns.eof()){
+			getline(interns, name,';');
+			getline(interns, correo,';');
+			getline(interns, password,';');
+			getline(interns, dias,';');
+			users.push_back(new Intern(name,correo,password,atoi(dias.c_str())));
+		}
+	}
+	interns.close();
+}
+void readSupervisors(){
 	string salario;
 	string name;
 	string correo;
